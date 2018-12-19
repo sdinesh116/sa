bb66ad1a6fc9b308b7ed2d8e114ce3848c431443
sdinesh116 <sdinesh161@gmail.com>
2018-12-19 08:18:40 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Configure Microsoft Azure Provider
+provider "azurerm" {
+	subscription_id = "${var.subscription_id}"
+}
+
+# Create Storage Account
+resource "azurerm_storage_account" "testsa" {
+	name 				= "${var.sa_name}"
+	location			= "${var.location}"
+	resource_group_name		= "${var.resource_group}"
+	account_tier			= "Standard"
+	account_replication_type 	= "LRS"
+}
+
+# Create a storage container
+resource "azurerm_storage_container" "container" {
+	name				= "${var.sa_name}-container"
+	resource_group_name		= "${var.resource_group}"
+	storage_account_name		= "${azurerm_storage_account.testsa.name}"
+	container_access_type		= "private"
+}

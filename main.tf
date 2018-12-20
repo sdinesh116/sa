58c1592bd3eda57e5cc4c75b64b81fea3290d1b5
sdinesh116 <sdinesh161@gmail.com>
2018-12-20 23:03:05 +0530

    chnages

--- main.tf
+++ main.tf
@@ -6,9 +6,19 @@ provider "azurerm" {
     tenant_id       = "${var.tenant_id}"
 }
 
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = "${azurerm_resource_group.rg.name}"
+    }
+
+    byte_length = 8
+}
+
 # Create Storage Account
 resource "azurerm_storage_account" "testsa" {
-	name 				= "${var.sa_name}"
+	name 				= "${var.sa_name}${random_id.randomId.hex}""
 	location			= "${var.location}"
 	resource_group_name		= "${var.resource_group}"
 	account_tier			= "Standard"

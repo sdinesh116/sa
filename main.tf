b4ba7ce20dcc295d51f72508304e1116e3ddf22a
sdinesh116 <sdinesh161@gmail.com>
2018-12-20 23:04:31 +0530

    chnages

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "random_id" "randomId" {
 
 # Create Storage Account
 resource "azurerm_storage_account" "testsa" {
-	name 				= "${var.sa_name}${random_id.randomId.hex}""
+	name 				= "${var.sa_name}${random_id.randomId.hex}"
 	location			= "${var.location}"
 	resource_group_name		= "${var.resource_group}"
 	account_tier			= "Standard"
